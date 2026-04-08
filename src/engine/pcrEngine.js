c9e707c20bb123a17ecbbadf5f6827eb51836d7e
bhaumiknaiksi-debug <bhaumiknaiksi@gmail.com>
2026-04-09 00:43:53 +0530

    Delete src/engine/pcrEngine.js

--- src/engine/pcrEngine.js
+++
@@ -1,9 +0,0 @@
-function calcPCR(chain) {
-  let totalCeOI = 0, totalPeOI = 0, totalCeVol = 0, totalPeVol = 0;
-  for (const row of chain.strikes) {
-    totalCeOI += row.ce.oi; totalPeOI += row.pe.oi;
-    totalCeVol += row.ce.volume; totalPeVol += row.pe.volume;
-  }
-  return { overall: { oiPCR: parseFloat((totalPeOI / (totalCeOI || 1)).toFixed(2)), volPCR: parseFloat((totalPeVol / (totalCeVol || 1)).toFixed(2)), totalCeOI, totalPeOI } };
-}
-module.exports = { calcPCR };
