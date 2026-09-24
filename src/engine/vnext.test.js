4ea364a7d0766aaa383e389bf6444429cb43be28
Bhaumik Naik <bhaumiknaiksi@gmail.com>
2026-09-24 14:22:54 +0530

    test: correct put-writing flow fixture

--- src/engine/vnext.test.js
+++ src/engine/vnext.test.js
@@ -16,7 +16,7 @@ const strikes = [
 ];
 
 const optionFlow = classifyOptionChainFlow([
-  { strike: 22900, ceOI: 120, cePrevOI: 100, ceLTP: 8, ceClosePrice: 10, peOI: 320, pePrevOI: 280, peLTP: 18, peClosePrice: 15 },
+  { strike: 22900, ceOI: 120, cePrevOI: 100, ceLTP: 8, ceClosePrice: 10, peOI: 320, pePrevOI: 280, peLTP: 13, peClosePrice: 15 },
   { strike: 23000, ceOI: 180, cePrevOI: 150, ceLTP: 12, ceClosePrice: 10, peOI: 240, pePrevOI: 270, peLTP: 9, peClosePrice: 12 }
 ], -1.2);
 assert(optionFlow.aggregate.classifiedContracts >= 2);
